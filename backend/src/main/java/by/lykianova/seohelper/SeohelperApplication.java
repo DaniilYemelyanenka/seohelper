db94c89fb97234e1af5165a7f2a2c3ca105097bd
DaniilYemelyanenka <whitesmurf0612@gmail.com>
2025-12-10 22:56:41 +0300

    init:initalize project

---
+++ backend/src/main/java/by/lykianova/seohelper/SeohelperApplication.java
@@ -0,0 +1,13 @@
+package by.lykianova.seohelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeohelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeohelperApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/by/lykianova/seohelper/SeohelperApplicationTests.java
@@ -0,0 +1,13 @@
+package by.lykianova.seohelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeohelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
